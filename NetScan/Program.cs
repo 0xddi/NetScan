959b2344aac5fe1c38171aaaefff1ce6ff0f19b5
0xddi <7778885@bk.ru>
2026-06-12 14:48:09 +0500

    initial commit

---
+++ NetScan/Program.cs
@@ -0,0 +1,12 @@
+﻿using SharpPcap;
+
+namespace NetScan;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ver = Pcap.SharpPcapVersion;
+        Console.WriteLine($"NetScan. Running on SharpPCAP {ver}");
+    }
+}
